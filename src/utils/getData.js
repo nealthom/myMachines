d3bfe28e95260d1a5efea6d51419c511c6c36986
Thomas Neal <nealthom@gmail.com>
2020-10-03 06:37:55 -0500

    testing

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const getData = async () => {
   try {
     const res = await axios.get(process.env.REACT_APP_URL);
-    console.log(process.env.REACT_APP_URL);
     return res.data;
   } catch (error) {
     return { error: "unable to get Data" };
